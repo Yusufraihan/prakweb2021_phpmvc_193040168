0b0f9c63297a7d5907409256d5238d8615f39781
R Yusuf Raihan Setiawan <yuraindo8@gmail.com>
2021-10-01 17:07:10 +0700

    menambahkan file constants.php di folder core dan menambahkan bootstrap pada halaman url kita

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,21 @@
+<div class="container">
 
-    <h1>About Me</h1>
-    <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur'] ?>, saya adalah seorang <?= $data['pekerjaan'] ?>. </p>
+    <div class="accordion" id="accordionExample">
+    <div class="accordion-item">
+        <h2 class="accordion-header" id="headingOne">
+        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        <h1>About Me</h1>
+        </button>
+        </h2>
+        <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+        <div class="accordion-body">
+        <img src="<?= BASEURL; ?>/img/profile.jpg" alt="Yusuf" width ="200" class="rounded-circle ">
+        <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur'] ?>, saya adalah seorang <?= $data['pekerjaan'] ?>. </p>
+        </div>
+        </div>
+    </div>
+</div>
+
+
+
+    
